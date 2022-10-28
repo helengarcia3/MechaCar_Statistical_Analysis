a7cb08bae85f992e191a947e7d6410e1f62d763b
helengarcia3 <107590196+helengarcia3@users.noreply.github.com>
2022-10-28 15:25:53 -0400

    Delete MechaCarChallenge.R

--- MechaCarChallenge.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-
-
-MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-
-multiple.MechaCar = lm(vehicle_length ~ vehicle_weight+ spoiler_angle + ground_clearance + AWD + mpg, data = MechaCar_mpg)
-summary(multiple.MechaCar)
